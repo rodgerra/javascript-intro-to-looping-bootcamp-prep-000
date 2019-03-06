08f5d4aca7be56b6ffbba3c05cf0e2de28b31e8b
Ranae Rodgers <ranae.rodgers@icloud.com>
2019-03-06 22:51:18 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,8 +1,7 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i+++) {
     if(i===1) {"I am 1 strange loop."}
-    else {`I am ${i} strange loops.`;
-    }
+    else {"I am ${i} strange loops."}
      return array;
   }
   
